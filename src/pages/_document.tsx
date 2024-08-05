aede32f05bafc32af9720bef65e4de8684d5d14d
Jonathan Araldi <jonathan.araldi@outlook.com>
2024-08-05 23:01:46 +0200

    first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,12 +2,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="fr">
       <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
     </Html>
   );
 }
