c2c2f9798751b3c636e56f784a3170c290036447
Jonathan Araldi <jonathan.araldi@outlook.com>
2024-08-06 17:47:58 +0200

    Mise à jour des dépendances du projet pour le déploiement de Firebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Seo } from "@/ui/components/Seo";
+import { Seo } from "@/ui/components/seo";
 
 export default function Home() {
   return (
     <>
       <Seo title="Coders Monkers" description="Description..." />
-      
+      <h1>Coders Monkeys wev app</h1>
     </>
   );
 }
